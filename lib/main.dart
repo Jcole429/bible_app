ba742fa9d1c6c53f34c75a1e9803310ad2083544
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2025-02-27 00:10:07 -0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/pages/app.dart';
+import 'package:flutter_tutorial/pages/bibles.dart';
+import 'package:flutter_tutorial/pages/notes.dart';
+import 'package:flutter_tutorial/pages/reader.dart';
+import 'package:flutter_tutorial/pages/search.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: App(),
+      routes: {
+        '/reader': (context) => ReaderPage(),
+        '/search': (context) => SearchPage(),
+        '/notes': (context) => NotesPage(),
+        '/bibles': (context) => BiblesScreen(),
+      },
+    );
+  }
+}
---
+++ lib/pages/bibles.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import '../services/api_service.dart';
+
+class BiblesScreen extends StatefulWidget {
+  const BiblesScreen({super.key});
+
+  @override
+  _BiblesScreenState createState() => _BiblesScreenState();
+}
+
+class _BiblesScreenState extends State<BiblesScreen> {
+  final ApiService apiService = ApiService();
+  late Future<List<dynamic>> futurePosts;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePosts = apiService.fetchPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Bibles")),
+      body: FutureBuilder<List<dynamic>>(
+        future: futurePosts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text("No posts found"));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index]['name']),
+                  subtitle: Column(
+                    children: [
+                      Text("id: ${snapshot.data![index]['id']}"),
+                      Text(
+                        "abbreviation: ${snapshot.data![index]['abbreviation']}",
+                      ),
+                      Text(
+                        "description: ${snapshot.data![index]['description']}",
+                      ),
+                      Html(data: snapshot.data![index]['info']),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
