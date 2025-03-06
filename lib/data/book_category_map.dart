a3525fb78f03d68a45ab02cade5fd5014ef7b8cd
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2025-03-06 14:57:46 -0300

    Add map for divisions of the bible

---
+++ lib/data/book_category_map.dart
@@ -0,0 +1,101 @@
+Map<int, dynamic> parentCategories = {
+  0: {"eng": "Old Testament"},
+  1: {"eng": "New Testament"},
+};
+
+Map<int, dynamic> categories = {
+  0: {"parent_category": 0, "eng": "Books of the Law"},
+  1: {"parent_category": 0, "eng": "Historical Books"},
+  2: {"parent_category": 0, "eng": "Poetic Books"},
+  3: {"parent_category": 0, "eng": "Major Prophetic Books"},
+  4: {"parent_category": 0, "eng": "Minor Prophetic Books"},
+  5: {"parent_category": 1, "eng": "The Gospels"},
+  6: {"parent_category": 1, "eng": "The Historical Book"},
+  7: {"parent_category": 1, "eng": "The Letters"},
+  8: {"parent_category": 1, "eng": "The Book of Vision"},
+};
+
+Map<String, dynamic> bookCategoryMap = {
+  "GEN": 0,
+  "EXO": 0,
+  "LEV": 0,
+  "NUM": 0,
+  "DEU": 0,
+  "JOS": 1,
+  "JDG": 1,
+  "RUT": 1,
+  "1SA": 1,
+  "2SA": 1,
+  "1KI": 1,
+  "2KI": 1,
+  "1CH": 1,
+  "2CH": 1,
+  "EZR": 1,
+  "NEH": 1,
+  "EST": 1,
+  "JOB": 2,
+  "PSA": 2,
+  "PRO": 2,
+  "ECC": 2,
+  "SNG": 2,
+  "ISA": 3,
+  "JER": 3,
+  "LAM": 3,
+  "EZK": 3,
+  "DAN": 3,
+  "HOS": 4,
+  "JOL": 4,
+  "AMO": 4,
+  "OBA": 4,
+  "JON": 4,
+  "MIC": 4,
+  "NAM": 4,
+  "HAB": 4,
+  "ZEP": 4,
+  "HAG": 4,
+  "ZEC": 4,
+  "MAL": 4,
+  "MAT": 5,
+  "MRK": 5,
+  "LUK": 5,
+  "JHN": 5,
+  "ACT": 6,
+  "ROM": 7,
+  "1CO": 7,
+  "2CO": 7,
+  "GAL": 7,
+  "EPH": 7,
+  "PHP": 7,
+  "COL": 7,
+  "1TH": 7,
+  "2TH": 7,
+  "1TI": 7,
+  "2TI": 7,
+  "TIT": 7,
+  "PHM": 7,
+  "HEB": 7,
+  "JAS": 7,
+  "1PE": 7,
+  "2PE": 7,
+  "1JN": 7,
+  "2JN": 7,
+  "3JN": 7,
+  "JUD": 7,
+  "REV": 8,
+};
+
+String getCategoryForBook(String bookId, String languageId) {
+  int bookCategoryId = bookCategoryMap[bookId];
+  dynamic category = categories[bookCategoryId];
+
+  return category[languageId];
+}
+
+String getParentCategoryForBook(String bookId, String languageId) {
+  dynamic category = getCategoryForBook(bookId, languageId);
+
+  int parentCategoryId = category['parent_category'];
+  dynamic parentCategory = parentCategories[parentCategoryId];
+
+  return parentCategory;
+}
